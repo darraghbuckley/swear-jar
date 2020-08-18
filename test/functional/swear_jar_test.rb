7c635a691fd6d5340b9d5da315b50694f2e2b440
Darragh Buckley <darragh.buckley@gmail.com>
2020-08-17 22:25:11 -0700

    Support BANK_ACCOUNT_ID environment variable. (#7)
    If the `BANK_ACCOUNT_ID` environment variable is provided, the account list isn't shown (or returned to the client). This allows for more public sharing of the swear jar because it doesn't show all the accounts and balances.

--- test/functional/swear_jar_test.rb
+++ test/functional/swear_jar_test.rb
@@ -9,11 +9,36 @@ describe SwearJar do
 
   describe '/' do
     it 'accepts a request' do
+      expect_any_instance_of(app).to receive(:bank_api_key)
+        .and_return('api_key')
       response_stub = instance_double(
         RestClient::Response,
-        body: [
-          { id: 'account0', name: 'Account', balance: 101 },
-        ].to_json,
+        body: {
+          data: [{ id: 'account0', name: 'Account', balance: 101 }],
+        }.to_json,
+      )
+      expect_any_instance_of(RestClient::Request).to receive(:execute)
+        .and_return(response_stub)
+      response = get '/'
+      expect(response.status).to eq(200)
+    end
+
+    it 'accepts a request without BANK_API_KEY' do
+      response = get '/'
+      expect(response.status).to eq(200)
+    end
+
+    it 'accepts a request with BANK_ACCOUNT_ID' do
+      account_id = 'account0'
+      expect_any_instance_of(app).to receive(:bank_api_key)
+        .and_return('api_key')
+      expect_any_instance_of(app).to receive(:bank_account_id)
+        .and_return(account_id)
+      response_stub = instance_double(
+        RestClient::Response,
+        body: {
+          data: [{ id: account_id, name: 'Account', balance: 101 }],
+        }.to_json,
       )
       expect_any_instance_of(RestClient::Request).to receive(:execute)
         .and_return(response_stub)
@@ -22,6 +47,8 @@ describe SwearJar do
     end
 
     it 'gracefully handles failures' do
+      expect_any_instance_of(app).to receive(:bank_api_key)
+        .and_return('api_key')
       response_stub = instance_double(
         RestClient::Response,
         body: {}.to_json,
@@ -54,6 +81,17 @@ describe SwearJar do
       )
       expect_any_instance_of(RestClient::Request).to receive(:execute)
         .and_return(response_stub)
+      response = post '/jar/swear'
+      expect(response.status).to eq(302)
+    end
+
+    it 'gracefully handles failures' do
+      response_stub = instance_double(
+        RestClient::Response,
+        body: {}.to_json,
+      )
+      expect_any_instance_of(RestClient::Request).to receive(:execute)
+        .and_raise(RestClient::ExceptionWithResponse, response_stub)
       response = post '/jar/swear'
       expect(response.status).to eq(302)
     end
